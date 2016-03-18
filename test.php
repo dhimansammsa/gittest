1640726f3d2bdc9390cfa0fdba483c5f5c1d2702
dhimansammsa <dhiman.sammsa@gmail.com>
2016-03-18 11:56:11 +0530

    Test Change

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+echo 'sssssss';
+echo 'lllllll';
+exit;
+// Get cURL resource
+/*$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt($curl, CURLOPT_URL,'http://69.2.50.16/kmrtest/');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_HEADER, 1);
+curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT     5.1)");
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+echo 'dddddd';
+print_r($resp);
+// Close request to clear up some resources
+curl_close($curl);*/
+
+?>
